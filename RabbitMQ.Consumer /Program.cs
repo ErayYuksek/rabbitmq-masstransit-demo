90c5f457db44755a587c926aef67bfe48d738bda
Eray Yuksek <erayyuksekk0606@gmail.com>
2026-02-12 21:49:10 +0300

    RabbitMQ publisher and consumer setup

---
+++ RabbitMQ.Consumer /Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Bağlantı oluşturma
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672/")
+    // Local ise:
+    // Uri = new Uri("amqp://guest:guest@localhost:5672/")
+};
+
+// Bağlantı aç
+using var connection = factory.CreateConnection();
+
+// Kanal aç
+using var channel = connection.CreateModel();
+
+// Queue oluştur (Publisher ile birebir aynı olmalı!)
+channel.QueueDeclare(
+    queue: "example-queue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+Console.WriteLine("Mesaj bekleniyor...");
+
+// Consumer oluştur
+var consumer = new EventingBasicConsumer(channel);
+
+// Kuyruktan mesaj dinleme
+channel.BasicConsume(
+    queue: "example-queue",
+    autoAck: true,
+    consumer: consumer
+);
+
+// Mesaj geldiğinde çalışacak event
+consumer.Received += (model, e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Gelen mesaj: {message}");
+};
+
+Console.ReadLine();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// Bağlantı oluşturma
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672/")
+    // CloudAMQP ise burayı değiştir:
+    // Uri = new Uri("amqps://KULLANICI:ŞİFRE@HOST/VHOST")
+};
+
+// Bağlantıyı aç
+using var connection = factory.CreateConnection();
+
+// Kanal aç
+using var channel = connection.CreateModel();
+
+// Queue oluştur
+channel.QueueDeclare(
+    queue: "example-queue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+// Mesajı byte[] haline çevir
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+
+// Mesaj gönder
+channel.BasicPublish(
+    exchange: "",
+    routingKey: "example-queue",
+    basicProperties: null,
+    body: message
+);
+
+Console.WriteLine("Mesaj gönderildi");
+Console.ReadLine();
